a8b37a739cc59983975f42b962a8f5abad4d45d2
David Cho <gcho13@my.bcit.ca>
2023-12-25 03:22:55 -0500

    Added Auth for Google and Github

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 // pages/_app.tsx
-import '../src/styles/globals.css';
-import React from 'react';
-import type { AppProps } from 'next/app';
+import "../src/styles/globals.css";
+import React from "react";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import { useSession, signIn, signOut } from "next-auth/react";
+import styles from "../src/styles/Login.module.css";
 
-const Login: React.FC = () => {
-    return (
-        <div>
-            {/* Login form goes here */}
-        </div>
-    );
-};
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
-export default Login;
+export default function Login() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <div>Loading</div>;
+  }
+
+  return (
+    <div className={styles.container}>
+      {!session ? (
+        <>
+          <button className={styles.button} onClick={() => signIn("github")}>
+            <FaGithub /> Sign in with GitHub
+          </button>
+          <button className={styles.button} onClick={() => signIn("google")}>
+            <FcGoogle /> Sign in with Google
+          </button>
+        </>
+      ) : (
+        <>
+          <p className={styles.welcome}>
+            Welcome, {session.user?.name ?? session.user?.email}!
+          </p>
+          <button className={styles.button} onClick={() => signOut()}>
+            Sign out
+          </button>
+        </>
+      )}
+    </div>
+  );
+}
